133b3274a554f45c67a79baf0a380d5ff146f595
Page <hedgaming93@gmail.com>
2024-01-24 00:53:50 +0700

    Make read_video.py able to read entire folder and change from hard coded input/output to reference parent directory

--- Source/Getdata/read_video.py
+++ Source/Getdata/read_video.py
@@ -1,24 +1,32 @@
 import cv2
+from pathlib import Path
 
-def save_frames(video_path):
-    cap = cv2.VideoCapture(video_path)
+parentDirectory = Path(__file__).parent
+inputDirectory = parentDirectory.joinpath("Input")
+outputDirectory = parentDirectory.joinpath("Output")
+supportsExtension = ["*/*.mp4", "*/*.mov"]
+videoPATHs = []
 
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-    success, frame = cap.read()
-    frame_number = 0
-
-    while success:
-        image_path = fr'C:\Users\max\OneDrive\Desktop\Signa-Link-1\Source\Getdata\Test\Image\frame_{frame_number:04d}.png' #ไปเปลี่ยนเอาเองนะฮาฟ
-        cv2.imwrite(image_path, frame)
-
+def save_frames(video_path, label):
+    try:    
+        cap = cv2.VideoCapture(str(video_path))
         success, frame = cap.read()
-        frame_number += 1
+        frame_number = 0
+        labelDirectory = outputDirectory.joinpath(label)
+        labelDirectory.mkdir(parents=True, exist_ok=True) #Create directory if not exist
 
-        if frame_number % 100 == 0:
-            print(f'Processed {frame_number} frames')
-            
-    print(f'\n\nProcessed {frame_number} frames')
-    cap.release()
+        while success:
+            image_path = str(labelDirectory.joinpath(video_path.name.split(".")[0] + "_" + str(frame_number) + ".png"))
+            cv2.imwrite(image_path, frame)
+            success, frame = cap.read()
+            frame_number += 1
+        cap.release()
+        print("Processed: " + str(video_path))
+    except:
+        print("Error occur on: " + video_path.name)
 
-save_frames(r'C:\Users\max\OneDrive\Desktop\Signa-Link-1\Source\Getdata\Test\Video\test.mp4') #ไปเปลี่ยนเอาเองนะฮาฟ
+for extension in supportsExtension: #Collect file that has mp4 and mov file extension
+    for file in inputDirectory.glob(extension):
+        videoPATHs.append(file)
+for videoPATH in videoPATHs:
+    save_frames(videoPATH, videoPATH.parent.name)
