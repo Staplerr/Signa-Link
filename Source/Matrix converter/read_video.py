ed971704250c1c61ac15a63982a37b7714336bad
Page <hedgaming93@gmail.com>
2024-02-05 22:46:15 +0700

    add multiprocess support to read_video.py

--- Source/Matrix converter/read_video.py
+++ Source/Matrix converter/read_video.py
@@ -1,32 +1,69 @@
 import cv2
 from pathlib import Path
+import time
+import multiprocessing
+import os
+import sys
 
 parentDirectory = Path(__file__).parent
 inputDirectory = parentDirectory.joinpath("Input")
 outputDirectory = parentDirectory.joinpath("Output")
 supportsExtension = ["*/*.mp4", "*/*.mov"]
-videoPATHs = []
+processes = []
+processesCount = 10
 
-def save_frames(video_path, label):
+def getFilePATHS(directory):
+    videoPATHs = []
+    for extension in supportsExtension: #Collect file that has mp4 and mov file extension
+        for file in directory.glob(extension):
+            videoPATHs.append(file)
+    print(f"total files: {len(videoPATHs)}")
+    return videoPATHs
+
+def splitList(list):
+    step = len(list) // processesCount
+    remain = len(list) % processesCount
+    for i in range(0, len(list), step):
+        yield list[i:i + step + remain] #multiple return 1D list
+        remain = 0
+
+def save_frames(videoPATHs):
     try:    
-        cap = cv2.VideoCapture(str(video_path))
-        success, frame = cap.read()
-        frame_number = 0
-        labelDirectory = outputDirectory.joinpath(label)
-        labelDirectory.mkdir(parents=True, exist_ok=True) #Create directory if not exist
-
-        while success:
-            image_path = str(labelDirectory.joinpath(video_path.name.split(".")[0] + "_" + str(frame_number) + ".png"))
-            cv2.imwrite(image_path, frame)
+        for videoPATH in videoPATHs:
+            cap = cv2.VideoCapture(str(videoPATH))
             success, frame = cap.read()
-            frame_number += 1
-        cap.release()
-        print("Processed: " + str(video_path))
-    except:
-        print("Error occur on: " + video_path.name)
+            frame_number = 0
+            labelDirectory = outputDirectory.joinpath(videoPATH.parent.name)
+            labelDirectory.mkdir(parents=True, exist_ok=True) #Create directory if not exist
 
-for extension in supportsExtension: #Collect file that has mp4 and mov file extension
-    for file in inputDirectory.glob(extension):
-        videoPATHs.append(file)
-for videoPATH in videoPATHs:
-    save_frames(videoPATH, videoPATH.parent.name)
+            while success:
+                image_path = str(labelDirectory.joinpath(videoPATH.name.split(".")[0] + "_" + str(frame_number) + ".png"))
+                cv2.imwrite(image_path, frame)
+                del frame
+                del success
+                success, frame = cap.read()
+                frame_number += 1
+            cap.release()
+            del frame_number
+            del cap
+    except:
+        pass
+
+startTime = time.perf_counter()
+
+videoPATHs = getFilePATHS(inputDirectory)
+videoPATHsList = list(splitList(videoPATHs))
+del videoPATHs #hopefully it will freeup some memory
+
+for i in range(processesCount): #initiate process
+    p = multiprocessing.Process(target=save_frames, args=[videoPATHsList[i]])
+    p.start()
+    processes.append(p)
+for process in processes: #wait for process to end
+    process.join()
+
+del videoPATHsList
+del processes
+
+finishTime = time.perf_counter()
+print(f"total time: {finishTime - startTime}")
