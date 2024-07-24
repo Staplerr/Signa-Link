ef8db2c8ae05fd61a80598e656fdfd433351936d
Page <hedgaming93@gmail.com>
2024-07-24 20:52:13 +0700

    Add train function and add hidden_size to lstm layer (untest)

--- Source/AI/PyTorch/Hand_Trainer.py
+++ Source/AI/PyTorch/Hand_Trainer.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as functional
+import torch.optim as optim
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -10,7 +10,8 @@ import glob
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-def readDataset(datasetPath: Path):
+def readDataset(datasetPath: Path,
+                splitRation: float):
     return
 
 class handLandmarkAnalyzer(nn.Module):
@@ -20,7 +21,7 @@ class handLandmarkAnalyzer(nn.Module):
         self.conv = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=(3, 3))
         self.maxPooling = nn.MaxPool2d(kernel_size=(2, 2))
         self.flattern = nn.Flatten()
-        self.LSTM1 = nn.LSTM(input_size=1260, num_layers=2)
+        self.LSTM1 = nn.LSTM(input_size=1260, hidden_size=8, num_layers=2)
         self.fcc1 = nn.Linear(in_features=1260, out_features=256)
         self.relu1 = nn.ReLU()
         self.fcc2 = nn.Linear(in_features=256, out_features=8)
@@ -37,12 +38,51 @@ class handLandmarkAnalyzer(nn.Module):
         x = self.softmax1(x)
         return x
 
-def trainModel(model, epoch, lossFunction, optimizer, trainData, testData):
-    pass
+def trainModel(model : nn.Module,
+               epoch : int,
+               lossFunction,
+               optimizer,
+               trainData,
+               validationData):
+    trainHistory = {
+        "trainLoss" : [],
+        "trainAccuracy" : [],
+        "evaluationLoss" : [],
+        "evaluationAccuracy" : []
+    }
+    for iteration in range(epoch + 1):
+        # Training
+        trainLoss, correct = 0, 0
+        model.train()
+        for data, label in trainData:
+            data, label = data.to(device), label.to(device)
+            prediction = model(data)
+            loss = lossFunction(prediction, data)
+
+            optimizer.zero_grad() # Reset loss from last iteration
+            loss.backward()
+            optimizer.step()
+            trainLoss += loss.item()
+            correct += (prediction.argmax(1) == label).float().sum().item()
+        trainHistory["trainLoss"].append(trainLoss / len(trainData))
+        trainHistory["trainAccuracy"].append(correct / len(trainData.dataset))
+
+        # Validation
+        validationLoss, correct = 0, 0
+        model.eval()
+        for data, label in validationData:
+            data, label = data.to(device), label.to(device)
+            prediction = model(data)
+            loss = lossFunction(prediction, data)
+            validationLoss += loss.item()
+            correct += (prediction.argmax(1) == label).float().sum().item()
+        trainHistory["evaluationLoss"].append(validationLoss / len(validationData))
+        trainHistory["evaluationAccuracy"].append(correct / len(validationData.dataset))
+    return model, trainHistory
 
 model = handLandmarkAnalyzer().to(device)
 lossFunction = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 batchSize = 32
 
 model, trainHistory = trainModel(model)
